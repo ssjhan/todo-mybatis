d16379612353aa643ee3f0a73032bbe18b9d7b18
ssjhan <jinseongseokjss@naver.com>
2022-12-31 18:55:41 +0900

    "주소, 전화번호 기능 추가"

--- src/main/java/com/example/demo/user/api/UserController.java
+++ src/main/java/com/example/demo/user/api/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.user.api;
 
+import com.example.demo.error.ErrorDTO;
 import com.example.demo.security.TokenProvider;
 import com.example.demo.user.dto.UserRequestDTO;
 import com.example.demo.user.dto.UserResponseDTO;
@@ -32,31 +33,40 @@ public class UserController {
 
             UserEntity user = userService.createServ(entity);
 
-            return ResponseEntity.ok().body(new UserResponseDTO(entity));
+            return ResponseEntity.ok().body(new UserResponseDTO(user));
 
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
+    // 로그인 요청 처리
     @PostMapping("/signin")
-    public ResponseEntity<?> signin(@RequestBody UserRequestDTO  dto ){
+    public ResponseEntity<?> signin(@RequestBody UserRequestDTO dto) {
         log.info("/auth/signin POST!! - login info : {}", dto);
 
         try {
-            UserEntity user =   userService.validateLogin(dto.getEmail(), dto.getPassword());
+            UserEntity user
+                    = userService.validateLogin(dto.getEmail(), dto.getPassword1());
 
-            //토큰 생성
+            // 토큰 생성
             final String token = provider.create(user);
 
-           UserResponseDTO responseDTO = new UserResponseDTO(user);
+            UserResponseDTO responseDTO = new UserResponseDTO(user);
             responseDTO.setToken(token); // 발행한 토큰을 응답정보에 포함
 
             return ResponseEntity.ok().body(responseDTO);
 
         } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ErrorDTO(e.getMessage()));
+        }
+    }
 
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }//ctrl + alt + t(여러가지 문법 자동생성 단축키(예 : try-catch))
+    // /auth/check?email=aaa@naver.com
+    @GetMapping("/check")
+    public ResponseEntity<?> checkEmail(String email) {
+        boolean flag = userService.isDuplicate(email);
+        log.info("{} 중복여부?? - {}", email, flag);
+        return ResponseEntity.ok().body(flag);
     }
 }
